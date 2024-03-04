d8c2a1c46aac92add52bb1a5d2cd8d2cb1d6c14f
Sai Sashreek D <sai@5thdt.com>
2024-03-04 18:38:23 +0530

    Title Update

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -18,22 +18,17 @@ const Cards = () => {
         />
 
         <header className="flex items-center justify-between leading-tight p-2 md:p-4">
-          <h1 className="text-lg no-underline hover:underline text-black">
-            Article Title
+          <h1 className="text-lg no-underline text-black">
+            Words Scramble
           </h1>
-          <p className="text-grey-darker text-sm">11/1/19</p>
+          <p className="text-teal-700 font-semibold text-sm">Play Now</p>
         </header>
 
         <footer className="flex items-center justify-between leading-none p-2 md:p-4">
-          <div className="flex items-center no-underline hover:underline text-black">
-            <img
-              alt="Placeholder"
-              className="block rounded-full"
-              src="https://picsum.photos/32/32/?random"
-            />
-            <p className="ml-2 text-sm">Author Name</p>
+          <div className="flex items-center no-underline text-black">
+            <p className="text-lg">Age: 6 - 10</p>
           </div>
-          <div className="no-underline text-grey-darker hover:text-red-dark">
+          <div className="no-underline text-teal-700 hover:text-red-dark">
             <span className="hidden">Like</span>
             <i className="fa fa-heart"></i>
           </div>
