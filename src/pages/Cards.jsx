c040a193617b22b2ad2a3868ac39fcce24c3697f
Sai Sashreek D <sai@5thdt.com>
2024-03-05 11:55:13 +0530

    Card Component

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -1,32 +1,23 @@
+/* eslint-disable react/prop-types */
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
-const Cards = () => {
-  const navigate = useNavigate();
-
-  const cardHandler = () => {
-    navigate('/game/words/');
-  };
-
+const Cards = ({ imgSrc, title, desc, onClick }) => {
   return (
     <>
-      <article className="overflow-hidden rounded-lg shadow-lg cursor-pointer" onClick={cardHandler}>
-        <img
-          alt="Placeholder"
-          className="block h-auto w-full"
-          src="https://picsum.photos/600/400/?random"
-        />
+      <article
+        className="overflow-hidden rounded-lg shadow-lg cursor-pointer"
+        onClick={onClick}
+      >
+        <img alt="Placeholder" className="block h-auto w-full" src={imgSrc} />
 
         <header className="flex items-center justify-between leading-tight p-2 md:p-4">
-          <h1 className="text-lg no-underline text-black">
-            Words Scramble
-          </h1>
+          <h1 className="text-lg no-underline text-black">{title}</h1>
           <p className="text-teal-700 font-semibold text-sm">Play Now</p>
         </header>
 
         <footer className="flex items-center justify-between leading-none p-2 md:p-4">
           <div className="flex items-center no-underline text-black">
-            <p className="text-lg">Age: 6 - 10</p>
+            <p className="text-lg">{desc}</p>
           </div>
           <div className="no-underline text-teal-700 hover:text-red-dark">
             <span className="hidden">Like</span>
