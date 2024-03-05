e447a5bfbfa4748b908a5d3f29212e49c5f42335
Sai Sashreek D <sai@5thdt.com>
2024-03-05 16:34:24 +0530

    Obj file imported

--- src/games/NumbersGame.jsx
+++ src/games/NumbersGame.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Main } from "../components/index";
 
 const NumbersGame = () => {
-  return (
-    <div>NumbersGame</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default NumbersGame
+  const btnHandler = () => {
+    navigate("/");
+  };
+
+  return (
+    <>
+      <button
+        type="button"
+        className="m-2 p-2 rounded-xl bg-gray-700 text-slate-300 hover:bg-gray-500 hover:text-slate-100 hover:text-lg font-karla font-semibold"
+        onClick={btnHandler}
+      >
+        Home
+      </button>
+      <Main />
+    </>
+  );
+};
+
+export default NumbersGame;
