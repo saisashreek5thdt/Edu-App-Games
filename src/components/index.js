1b4dc619affe86ba608bae32bf470cdda23ff524
Sai Sashreek D <sai@5thdt.com>
2024-03-05 16:34:08 +0530

    Numbers Game JS to Obj

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 export { default as Board } from "./Board";
 export { default as Keyboard } from "./Keyboard";
 export { default as GameOver } from "./GameOver";
+
+// Numbers Game
+export { default as Main } from "./Numbers/Main";
+export { default as Dice } from "./Numbers/Dice";
